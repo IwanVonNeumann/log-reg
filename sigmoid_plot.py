6dccce3f6419d9407011d5d2c1e9c0be889da16e
IwanVonNeumann <grabageheap@inbox.lv>
2019-01-29 21:32:50 +0200

    title added to sigmoid plot

--- sigmoid_plot.py
+++ sigmoid_plot.py
@@ -10,6 +10,8 @@ z = np.arange(-7, 7, 0.1)
 phi_z = sigmoid(z)
 
 plt.plot(z, phi_z)
+
+plt.title('Sigmoid')
 plt.axvline(0.0, color='k')
 plt.axhspan(0.0, 1.0, facecolor='1.0', edgecolor="black", ls='dotted')
 plt.axhline(y=0.5, ls='dotted', color='k')
